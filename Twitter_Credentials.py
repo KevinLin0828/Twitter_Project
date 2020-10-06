449f8f3cc20a1c5f053c423203183c74135295e2
Kevin Lin <32885225+KevinLin0828@users.noreply.github.com>
2020-10-06 15:26:56 -0400

    Delete Twitter_Credentials.py

--- Twitter_Credentials.py
+++
@@ -1,6 +0,0 @@
-# This file contains access token and api key in order to access live tweets on twitter
-
-ACCESS_TOKEN = "1306956803016716291-lPo2Ik24hKA0Z4XkuZ7Fr9ouWThNJR"
-ACCESS_TOKEN_SECRET = "VnNOgzjBa15Y8Hhkxh1fjEWJeYvFf4k7lCN6Padu83JFq"
-API_KEY = "BsjEuEtkcxPqL4MVIj169m7Yb"
-API_KEY_SECRET = "7Yuaaw0F6O1hmabcHktBahShJyaSzqnCzXHecHlyXqT9tOVfxl"
