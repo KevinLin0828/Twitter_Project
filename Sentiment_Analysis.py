96d3758bd350eaddc0788879840fc54a6d45817e
Kevin Lin <32885225+KevinLin0828@users.noreply.github.com>
2020-10-27 11:51:24 -0400

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -10,6 +10,10 @@ class Analysis:
         read = pd.read_csv("filtered_tweets.csv") # reads in CSV file
         new_column = pd.DataFrame(read['text']) # selects the text column and puts it into a data frame
         new_column.insert(0, 'name', read['name']) # inserts column called name into the data frame
+        new_column.insert(1, 'followers count',read['followers count']) # inserts column called followers count into the data frame
+        new_column.insert(2, 'friends count',read['friends count']) # inserts column called followers count into the data frame
+        new_column.insert(3, 'retweet count', read['retweet count'])  # inserts column called retweet count into data frame
+        new_column.insert(4, 'favorite count', read['favorite count']) # inserts column called favorite count into data frame
         new_column['polarity'] = new_column.apply(lambda x: TextBlob(x['text']).sentiment.polarity, axis=1) # Inserts a new column by sentimental analysis of the text column
         new_column['subjectivity'] = new_column.apply(lambda x: TextBlob(x['text']).sentiment.subjectivity, axis=1) # Inserts a new column by sentimental analysis of the text column
         new_column.to_csv('data_privacy_sentiment_analysis.csv', index=False) # puts the dataframe into a new csv file
