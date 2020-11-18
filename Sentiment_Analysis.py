8c82c26418f7a6e26d7c476f6d59285164997a7c
Kevin Lin <32885225+KevinLin0828@users.noreply.github.com>
2020-11-18 14:44:46 -0500

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -16,6 +16,10 @@ class Analysis:
         new_column.insert(4, 'favorite count', read['favorite count']) # inserts column called favorite count into data frame
         new_column['polarity'] = new_column.apply(lambda x: TextBlob(x['text']).sentiment.polarity, axis=1) # Inserts a new column by sentimental analysis of the text column
         new_column['subjectivity'] = new_column.apply(lambda x: TextBlob(x['text']).sentiment.subjectivity, axis=1) # Inserts a new column by sentimental analysis of the text column
+        polarity_range_df = new_column['polarity'].between(-0.2, 0.2, inclusive=True) # searches for tweets that are in the range of -0.2 to 0.2 in polarity.
+        subject_range_df = new_column['subjectivity'].between(0.4, 0.6, inclusive=True) # searches for tweets that are in the range of 0.4 to 0.6 in subjectivity.
+        new_column.insert(7, 'polarity between -0.2 to 0.2', polarity_range_df) # inserts the polarity range into data frame
+        new_column.insert(9, 'subjectivity between 0.4 to 0.6', subject_range_df) # inserts the subjectivity range into data frame
         new_column.to_csv('data_privacy_sentiment_analysis.csv', index=False) # puts the dataframe into a new csv file
     
     #this method plots a scatter plot based on polarity, favorites, retweets and subjectivity.
@@ -62,6 +66,13 @@ class Analysis:
         axs[1].set_xlabel('subjectivity')
         plt.tight_layout()
         plt.show()
+    
+    #This method shows the tweets that contain in the range of -0.2 to 0.2 in polarity and of 0.4 to 0.6 in subjectivity.
+    def polarity_subjectivity_range(self):
+        data = pd.read_csv("data_privacy_sentiment_analysis.csv",  dtype={'polarity between -0.2 to 0.2': str,'subjectivity between 0.4 to 0.6': str})
+        df = pd.DataFrame(data)
+        newdata = df[df['polarity between -0.2 to 0.2'].str.contains('True') & df['subjectivity between 0.4 to 0.6'].str.contains('True')]
+        newdata.to_csv('polarity_and _subjectivity_range.csv', index=False)
         
 if __name__ == "__main__":
     analysis = Analysis()
